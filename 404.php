d8831c90de5a9cea6ecde19d52f2b37cbf696098
Michael McMahon <mmcmahon70@southhills.edu>
2020-01-22 10:46:15 -0500

    editNav

--- 404.php
+++ 404.php
@@ -19,8 +19,11 @@ get_header();
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'uppercase' ); ?></p>
-					<img title="" class="scale-with-grid" alt="" src="/assets/images/compass-ValentinAntonucc-pexels.png">
+					
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links Above?', 'uppercase' ); ?></p>
+					
+				
+				<img src="assets/images/compass-ValentinAntonucc-pexels.png" alt="Compass">
 					
 
 					
