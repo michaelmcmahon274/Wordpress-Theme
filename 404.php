31740ff7512f85e5d42365f15ab67f1a76b5c691
Michael McMahon <mmcmahon70@southhills.edu>
2020-01-22 10:28:40 -0500

    errorPageChanges

--- 404.php
+++ 404.php
@@ -23,28 +23,7 @@ get_header();
 					<img title="" class="scale-with-grid" alt="" src="/assets/images/compass-ValentinAntonucc-pexels.png">
 					
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'uppercase' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$uppercase_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'uppercase' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$uppercase_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
