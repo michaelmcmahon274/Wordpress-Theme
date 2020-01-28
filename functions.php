b9f7a85bb0b76b940f3e2c99b3e402a8d0e297f2
Michael McMahon <mmcmahon70@southhills.edu>
2020-01-28 08:48:17 -0500

    addThemesupport

--- functions.php
+++ functions.php
@@ -79,6 +79,9 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
+
+		add_theme_support( 'post-formats',  array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+
 	}
 endif;
 add_action( 'after_setup_theme', 'uppercase_setup' );
